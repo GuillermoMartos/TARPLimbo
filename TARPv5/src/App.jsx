3f07b573efc8e8ead49966fe612dbf412df3ec12
GuillermoMartos <guille.l.martos@gmail.com>
2022-03-28 19:19:19 -0300

    avances en landing page

--- TARPv5/src/App.jsx
+++ TARPv5/src/App.jsx
@@ -42,16 +42,45 @@ function App() {
         />
 
         <select name="selector" id="selector" className="landing-select">
-            <option disabled selected>Network{"  "}</option>
-            <option>AAA</option>
-            <option>BBB</option>
-            <option>CCC</option>
+          <option disabled selected>Network{"  "}</option>
+          <option>AAA</option>
+          <option>BBB</option>
+          <option>CCC</option>
 
         </select>
         <button className='btn-select'>Read →</button>
-
-        
       </div>
+
+      <div className="stats">
+        <div className="box-stats">
+
+          <div className="titlestats">
+
+            <span className='more-stats'>●●●</span>
+            <h3>Contracts Analyzed</h3>
+          </div>
+          <h1>1.23k</h1>
+          <p>More than 200 this week</p>
+        </div>
+        <div className="box-stats">
+          <div className="titlestats">
+            <span className='more-stats'>●●●</span>
+            <h3>Scams detected</h3>
+          </div>
+          <h1>431</h1>
+          <p>7 scams detected this week</p>
+        </div>
+      </div>
+
+
+      <div className='phrase'>
+
+        <p>      Creating an account grants you the benefit of tracking your crypto portfolio and give you the possibility to receive notifications when a token you’re invested in, changes certain code inside the contract or when the liquidity lock is expired.</p>
+
+      </div>
+
+
+
     </div>
   )
 }
