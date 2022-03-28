cf584fb841c81b8ff52c72ed6df136245aecea29
Guillermo L. Martos <guille.l.martos@gmail.com>
2022-03-28 14:55:05 -0300

    dumb component header

--- TARPv5/src/App.jsx
+++ TARPv5/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Header from './Header/header'
 
 function App() {
 
 
   return (
     <div className="container">
+      <Header/>
       <h1>hola</h1>
 
     </div>
